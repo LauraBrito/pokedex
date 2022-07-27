d7fba56ea2fc94f2103c3655f62b743471177b92
Laura Brito <laurabritolisboa@gmail.com>
2022-07-27 11:42:31 -0300

    initial commit
    initial commit

---
+++ src/app/pokemom-list/pokemom-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PokemomListComponent } from './pokemom-list.component';
+
+describe('PokemomListComponent', () => {
+  let component: PokemomListComponent;
+  let fixture: ComponentFixture<PokemomListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PokemomListComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PokemomListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pokemom-list/pokemom-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemom-list',
+  templateUrl: './pokemom-list.component.html',
+  styleUrls: ['./pokemom-list.component.scss']
+})
+export class PokemomListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
